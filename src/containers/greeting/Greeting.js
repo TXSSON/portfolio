fe76438677b8be51aab77e3ac4471d7898784509
SON <txuanson95@gmai.com>
2025-11-12 20:28:56 +0700

    move file resume.pdf to public

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href="/resume.pdf" target="_blank" rel="noopener noreferrer"
                     download="Resume.pdf"
                     className="download-link-button"
                   >
